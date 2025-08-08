b7cf98e7923b7935eae066c0782ecf5b995c3609
Prakash <prakas.rj@gmail.com>
2025-08-07 19:26:15 -0500

    New VM 03

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ PROJECT_ID="lab03-467223"
 
 gcloud config set project $PROJECT_ID
 
-gcloud deployment-manager deployments update my-first-vm --template vm_template.jinja --project $PROJECT_ID --properties="timestamp:$(date +%s)" || \
-  gcloud deployment-manager deployments create my-first-vm --template vm_template.jinja --project $PROJECT_ID --properties="timestamp:$(date +%s)"
+gcloud deployment-manager deployments update my-first-vm03 --template vm_template.jinja --project $PROJECT_ID --properties="timestamp:$(date +%s)" || \
+  gcloud deployment-manager deployments create my-first-vm03 --template vm_template.jinja --project $PROJECT_ID --properties="timestamp:$(date +%s)"
